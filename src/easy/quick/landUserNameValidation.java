de2b98879bd77a7e1add50b687103ce0694e89ad
Melcn <melaniearinosy@outlook.com>
2023-07-04 18:43:21 +0200

    create landusername class

---
+++ src/easy/quick/landUserNameValidation.java
@@ -0,0 +1,10 @@
+package easy.quick;
+
+public class landUserNameValidation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Jekyll
+ *
+ */
+module muscle {
+}
