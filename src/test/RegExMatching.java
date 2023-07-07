e5e1464a0e45908b07e4a134f0ea05225ba40e73
Melcn <melaniearinosy@outlook.com>
2023-07-07 19:16:50 +0200

    fix codelanduservalidation regex condition + clean main

--- src/test/RegExMatching.java
+++ src/test/RegExMatching.java
@@ -4,7 +4,8 @@ public class RegExMatching {
 
 	public static boolean isValidEmail( String email) {
 		//String regExp = "^.+@.+\\..+$";
-		String regExp = "^[A-Za-z0-9._-]+@.+\\.[a-z]{2,}$";
+		//String regExp = "^[A-Za-z0-9._-]+@.+\\.[a-z]{2,}$";
+		String regExp = "^[\\w.-]+@.+[\\w.-]+\\.[a-z]{2,}$";
 		return email.matches(regExp);
 	}
 	
