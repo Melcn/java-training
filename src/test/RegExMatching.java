1395147f5c884dde67be1fe43d45fed7452ac1b0
Melcn <melaniearinosy@outlook.com>
2023-07-07 18:21:16 +0200

    create isValidEmail method

--- src/test/RegExMatching.java
+++ src/test/RegExMatching.java
@@ -2,4 +2,14 @@ package test;
 
 public class RegExMatching {
 
+	public static boolean isValidEmail( String email) {
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		 System.out.println(isValidEmail( "martin@societe.com" ));
+		 System.out.println(isValidEmail( "martin.societe.com" ));
+		 System.out.println(isValidEmail( "martin@societe"));
+		 
+	}
 }
