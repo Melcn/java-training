e6e30c64a7b8157397ebdb7ad573c9a9f2a51187
Melcn <melaniearinosy@outlook.com>
2023-07-08 10:18:35 +0200

    create findintersection class

---
+++ src/easy/quick/FindIntersection.java
@@ -0,0 +1,19 @@
+package easy.quick;
+
+import java.util.Scanner;
+
+public class FindIntersection {
+
+	public static String FindIntersection(String[] strArr) {
+		return strArr[0];
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println(FindIntersection(s.nextLine()));
+
+	}
+
+}
