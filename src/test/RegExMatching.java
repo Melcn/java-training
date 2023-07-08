1c32d1821f69c1b837ee16f7a9999f0e499e5972
Melcn <melaniearinosy@outlook.com>
2023-07-08 09:46:33 +0200

    create isvaliddate method

--- src/test/RegExMatching.java
+++ src/test/RegExMatching.java
@@ -2,17 +2,21 @@ package test;
 
 public class RegExMatching {
 
-	public static boolean isValidEmail( String email) {
-		//String regExp = "^.+@.+\\..+$";
-		//String regExp = "^[A-Za-z0-9._-]+@.+\\.[a-z]{2,}$";
+	public static boolean isValidDate(String date) {
+		return false;
+	}
+
+	public static boolean isValidEmail(String email) {
+		// String regExp = "^.+@.+\\..+$";
+		// String regExp = "^[A-Za-z0-9._-]+@.+\\.[a-z]{2,}$";
 		String regExp = "^[\\w.-]+@.+[\\w.-]+\\.[a-z]{2,}$";
 		return email.matches(regExp);
 	}
-	
+
 	public static void main(String[] args) {
-		 System.out.println(isValidEmail( "martin@societe.com" ));
-		 System.out.println(isValidEmail( "martin.societe.com" ));
-		 System.out.println(isValidEmail( "martin@societe"));
-		 
+		System.out.println(isValidEmail("martin@societe.com"));
+		System.out.println(isValidEmail("martin.societe.com"));
+		System.out.println(isValidEmail("martin@societe"));
+
 	}
 }
