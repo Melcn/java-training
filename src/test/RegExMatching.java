cf48858ca4bef7915f2bc3438418a8a558664fc6
Melcn <melaniearinosy@outlook.com>
2023-07-08 10:08:27 +0200

    create isValidIp method

--- src/test/RegExMatching.java
+++ src/test/RegExMatching.java
@@ -2,9 +2,16 @@ package test;
 
 public class RegExMatching {
 
+	// regular-expressions.info
+
+	public static boolean isValidIp(String ip) {
+		//192.168.100.10
+		return false;
+	}
+
 	public static boolean isValidDate(String date) {
-		//String regExp = "^\\d\\d/\\d\\d/\\d\\d(\\d\\d)?$";
-		//String regExp = "^[0_9]{2}/[0_9]{2}/[0_9]{2}([0_9]{2})?$";
+		// String regExp = "^\\d\\d/\\d\\d/\\d\\d(\\d\\d)?$";
+		// String regExp = "^[0_9]{2}/[0_9]{2}/[0_9]{2}([0_9]{2})?$";
 		String regExp = "^{0[1-9]|[12][0-9]|3[01]}/(0[1-9]|1[0-2])/(19|20)?([0-9]{2})$"; // jj/mm/aaaa || jj/mm/aa
 		return date.matches(regExp);
 	}
