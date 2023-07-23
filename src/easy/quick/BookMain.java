29ba580f1994f6ab1f140b562079d3e4224e343c
Melcn <melaniearinosy@outlook.com>
2023-07-23 12:20:34 +0200

    create showmenu method

--- src/easy/quick/BookMain.java
+++ src/easy/quick/BookMain.java
@@ -6,7 +6,18 @@ public class BookMain {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		sc.close();
+	}
+	
+	public static void showMenu() {
+
+		System.out.println("----Que souhaitez vous faire?----");
+		System.out.println("-------1. Ajouter un livre-------");
+		System.out.println("------2. Rechercher un livre-----");
+		System.out.println("-------3. Modifier un livre------");
+		System.out.println("------4. Supprimer un livre------");
+		System.out.println("-----5. Consulter les livres-----");
+		System.out.println("-----------0. Quitter------------");
 	}
 }
