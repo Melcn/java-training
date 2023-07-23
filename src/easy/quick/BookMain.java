420f01ddc5cf264391e09affa8625e43c7bbc50e
Melcn <melaniearinosy@outlook.com>
2023-07-23 12:25:28 +0200

    create obj + arraytest

--- src/easy/quick/BookMain.java
+++ src/easy/quick/BookMain.java
@@ -2,14 +2,29 @@ package easy.quick;
 
 import java.util.Scanner;
 
+import Book.Book;
+
 public class BookMain {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		Book books[] = new Book[3];
+		int choice = 0;
+		String enterTitle;
+		String newTitle;
+
+		books[0] = new Book(1, "Underground", "Haruki Murikami", 15);
+		books[1] = new Book(2, "L'alchimiste", "Paulo Coelho", 4);
+		books[2] = new Book(3, "Madame Bovary", "Flaubert", 2);
+
+		Book book = new Book();
+
+		showMenu();
+		choice = sc.nextInt();
 		sc.close();
 	}
-	
+
 	public static void showMenu() {
 
 		System.out.println("----Que souhaitez vous faire?----");
