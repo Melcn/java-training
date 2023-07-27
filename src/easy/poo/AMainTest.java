e42f314e0522092f153fecf66f63c730a0176c62
Melcn <melaniearinosy@outlook.com>
2023-07-28 01:04:03 +0200

    create showMenu in main class

--- src/easy/poo/AMainTest.java
+++ src/easy/poo/AMainTest.java
@@ -1,9 +1,21 @@
 package easy.poo;
 
+import java.util.Scanner;
+
 public class AMainTest {
 
 	public static void main(String[] args) {
-		
+
+		Scanner sc = new Scanner(System.in);
+		showMenu();
+		sc.close();
 	}
 
+	public static void showMenu() {
+
+		System.out.println("Que souhaitez vous faire");
+		System.out.println("1. Embaucher un employé");
+		System.out.println("2. Augmenter tous les employés");
+		System.out.println("3. Augmenter un employé");
+	}
 }
