8277132690d8092c957f1264571ddbfb0f4d5b36
Melcn <melaniearinosy@outlook.com>
2023-07-27 16:04:25 +0200

    create: Integerarray Class + fill/show method

---
+++ src/easy/quick/IntegerArray.java
@@ -0,0 +1,22 @@
+package easy.quick;
+
+import java.util.Scanner;
+
+public class IntegerArray {
+
+	public static void main(String[] args) {
+		int[] t = new int[6];
+
+		fill();
+		show();
+	}
+
+	public static void show() {
+		System.out.println("fill the blank");
+	}
+
+	public static void fill() {
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+	}
+}
