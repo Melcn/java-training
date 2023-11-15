59ce0bf709b1674808d3c5bd09578f40b3ef7f59
Melcn <melaniearinosy@outlook.com>
2023-11-15 16:13:37 +0100

    feat(wotraining): create statistics

---
+++ src/WOtraining/Statistics.java
@@ -0,0 +1,29 @@
+package WOtraining;
+
+public class Statistics {
+
+    private int count;
+    private int sum;
+
+    public Statistics() {
+        this.count = count;
+        this.sum = sum;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+}
