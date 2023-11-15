4b8b32d8e0c5ae3b39554eb32f373eeb3c9a6890
Melcn <melaniearinosy@outlook.com>
2023-11-15 10:50:15 +0100

    feat(quick): create new BufferedReader keyboard

--- src/easy/quick/FeuEnumType.java
+++ src/easy/quick/FeuEnumType.java
@@ -1,11 +1,16 @@
 package easy.quick;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class FeuEnumType {
 
+    static BufferedReader keyboard = new BufferedReader( new InputStreamReader(System.in))
     public static void main(String[] args) throws Exception {
 
         Feu feu = Feu.VERT;
+
+        System.out.println("Que voulez vous faire? ");
+        String action = keyboard.readLine();
     }
 }
