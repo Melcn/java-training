666f9f1331a3963e04fb253c48b6f1bd8b842f84
Melcn <melaniearinosy@outlook.com>
2023-11-15 15:04:47 +0100

    feat(wotraining): create Debt

---
+++ src/WOtraining/Debt.java
@@ -0,0 +1,11 @@
+package WOtraining;
+
+public class Debt {
+
+    private double initialBalance;
+    private double initialInterestRate;
+
+    public Debt(double initialBalance, double initialInterestRate){
+
+    }
+}
