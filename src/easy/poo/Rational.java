5250e5362335152d3212ff637f9830cf756b7f3e
Melcn <melaniearinosy@outlook.com>
2023-11-15 13:33:52 +0100

    feat(poo): create Rational properties

--- src/easy/poo/Rational.java
+++ src/easy/poo/Rational.java
@@ -5,4 +5,19 @@ public class Rational {
     private int numerator;
     private int denominateur;
 
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominateur() {
+        return denominateur;
+    }
+
+    public void setDenominateur(int denominateur) {
+        this.denominateur = denominateur;
+    }
 }
