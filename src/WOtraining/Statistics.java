f189522378f6c21521be47d56d7b5d873148e453
Melcn <melaniearinosy@outlook.com>
2023-11-15 16:17:00 +0100

    feat(wotraining): delete getcount in statistic class

--- src/WOtraining/Statistics.java
+++ src/WOtraining/Statistics.java
@@ -14,6 +14,10 @@ public class Statistics {
         return count;
     }
 
+    public void setCount(int count) {
+        this.count = count;
+    }
+
     public int getSum() {
         return sum;
     }
@@ -23,15 +27,8 @@ public class Statistics {
     }
 
     public void addNumber(int number) {
-        // write code here
-    }
 
-    public int getCount() {
-        // write code here
-    }
 
-    public void setCount(int count) {
-        this.count = count;
     }
 
     public int sum() {
--- src/WOtraining/StatisticsTest.java
+++ src/WOtraining/StatisticsTest.java
@@ -4,6 +4,13 @@ public class StatisticsTest {
 
     public static void main(String[] argv) {
 
-        
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
 }
