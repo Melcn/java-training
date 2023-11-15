6a8611b87588926217c9f5f32377060d612e61a0
Melcn <melaniearinosy@outlook.com>
2023-11-15 13:35:15 +0100

    feat(poo): test Rational object

--- src/easy/poo/RationalTest.java
+++ src/easy/poo/RationalTest.java
@@ -4,5 +4,10 @@ public class RationalTest {
 
     public static void main(String[] arg){
 
+        Rational r = new Rational();
+        r.setDenominateur(0);
+        r.setNumerator(3);
+
+        System.out.println();
     }
 }
