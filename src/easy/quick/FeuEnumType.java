fe5fb9dc924d92034c17e665a30860256e2268b9
Melcn <melaniearinosy@outlook.com>
2023-11-15 10:47:19 +0100

    feat(quick): test feu in main

--- src/easy/quick/FeuEnumType.java
+++ src/easy/quick/FeuEnumType.java
@@ -1,5 +1,11 @@
 package easy.quick;
 
+import java.io.BufferedReader;
+
 public class FeuEnumType {
 
+    public static void main(String[] args) throws Exception {
+
+        Feu feu = Feu.VERT;
+    }
 }
