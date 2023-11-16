f2ca3c8fa64d3f28d61a2904c664da48f17aadc5
Melcn <melaniearinosy@outlook.com>
2023-11-16 21:09:09 +0100

    feat(): init constructor in Clock class

--- src/WOtraining/Clock.java
+++ src/WOtraining/Clock.java
@@ -2,5 +2,14 @@ package WOtraining;
 
 public class Clock {
 
-    
+    private ClockHand hours;
+    private ClockHand minutes;
+    private ClockHand seconds;
+
+    public Clock() {
+        this.hours = new ClockHand(24);
+        this.minutes = new ClockHand(60);
+        this.seconds = new ClockHand(60);
+    }
+
 }
