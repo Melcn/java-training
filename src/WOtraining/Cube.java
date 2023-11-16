5872b45f1a3ac960fe4770a3a0b47305d4e35d73
Melcn <melaniearinosy@outlook.com>
2023-11-16 21:54:23 +0100

    feat(): create volume() in Cube class

--- src/WOtraining/Cube.java
+++ src/WOtraining/Cube.java
@@ -3,4 +3,12 @@ package WOtraining;
 public class Cube {
 
     private int edgeLength;
+
+    public Cube(int edgeLength) {
+        this.edgeLength = edgeLength;
+    }
+
+    public int volume() {
+        return edgeLength * edgeLength * edgeLength;
+    }
 }
