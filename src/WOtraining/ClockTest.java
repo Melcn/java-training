5f91e77d2743f32f69c1d7f44db597ae4b87bda8
Melcn <melaniearinosy@outlook.com>
2023-11-16 21:02:02 +0100

    feat(): init main clocktest"

--- src/WOtraining/ClockTest.java
+++ src/WOtraining/ClockTest.java
@@ -1,4 +1,11 @@
 package WOtraining;
 
 public class ClockTest {
+
+    public static void main(String[] argv) {
+
+        ClockHand hours = new ClockHand(24);
+        ClockHand minutes = new ClockHand(60);
+        ClockHand seconds = new ClockHand(60);
+    }
 }
