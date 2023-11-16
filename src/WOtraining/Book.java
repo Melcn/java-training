60d2f40d3f9b30d3dc4135f542b16b2e5d18e4e1
Melcn <melaniearinosy@outlook.com>
2023-11-16 21:44:31 +0100

    feat(): create Book class

---
+++ src/WOtraining/Book.java
@@ -0,0 +1,13 @@
+package WOtraining;
+
+public class Book {
+
+    private String author;
+    private String name;
+    private String pages;
+
+    public Book(String author, String name, int pages) {
+        this.author = author;
+        this.name = name;
+    }
+}
