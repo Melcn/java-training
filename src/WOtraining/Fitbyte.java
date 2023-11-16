9fa57c1ba86cc5db0c9fd967336890a82e443b0d
Melcn <melaniearinosy@outlook.com>
2023-11-16 22:02:31 +0100

    feat(): create Fitbyte class

---
+++ src/WOtraining/Fitbyte.java
@@ -0,0 +1,12 @@
+package WOtraining;
+
+public class Fitbyte {
+
+    private int age;
+    private int restingHeratRate;
+
+    public Fitbyte(int age, int restingHeartRate) {
+        this.age = age;
+        this.restingHeratRate = restingHeartRate;
+    }
+}
