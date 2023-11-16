959d486acf0ae4b4656d113e4248dfbfd18cb575
Melcn <melaniearinosy@outlook.com>
2023-11-16 21:35:01 +0100

    feat(): update attribute & constructor

--- src/WOtraining/Person.java
+++ src/WOtraining/Person.java
@@ -3,10 +3,10 @@ package WOtraining;
 public class Person {
     private String name;
     private int age;
-    private int weight;
-    private int height;
+    private double weight;
+    private double height;
 
-    public Person(String name) {
+    public Person(String name, int age, double weight, double height) {
         this.name = name;
         this.age = age;
         this.weight = weight;
