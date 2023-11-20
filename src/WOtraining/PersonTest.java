12dd4b91216950cb5a7fa78b418e6773062f950b
Melcn <melaniearinosy@outlook.com>
2023-11-20 11:59:43 +0100

    feat(wotraining): test person

--- src/WOtraining/PersonTest.java
+++ src/WOtraining/PersonTest.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class PersonTest {
 
     public static void main(String[] args) {
-        Person paul = new Person("Paul", 24);
+        Person paul = new Person("Paul", 24, 184, 70);
+        Person joan = new Person("Joan Ball");
         System.out.println(paul);
 
         paul.growOlder();
