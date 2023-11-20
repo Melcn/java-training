8fe761433fb446ad5527e155b93a506a79cf3621
Melcn <melaniearinosy@outlook.com>
2023-11-20 16:05:32 +0100

    feat(wotrainig): test eatHealthy()

--- src/WOtraining/PaymentTest.java
+++ src/WOtraining/PaymentTest.java
@@ -22,8 +22,8 @@ public class PaymentTest {
         change = unicafeExactum.eatAffordably(5);
         System.out.println("remaining change " + change);
 
-        //change = unicafeExactum.eatHeartily(4.3);
-        //System.out.println("remaining change " + change);
+        change = unicafeExactum.eatHeartily(4.3);
+        System.out.println("remaining change " + change);
 
         System.out.println(unicafeExactum);
     }
