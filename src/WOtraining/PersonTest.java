49070d1d6012489c4c86a71072782253aa85c56c
Melcn <melaniearinosy@outlook.com>
2023-11-20 09:42:57 +0100

    feat(wotrainig): TEST surcharge growolder() in person

--- src/WOtraining/PersonTest.java
+++ src/WOtraining/PersonTest.java
@@ -5,18 +5,13 @@ import java.util.Scanner;
 public class PersonTest {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
+        Person paul = new Person("Paul", 24);
+        System.out.println(paul);
 
-        System.out.println("What's your name?");
-        String name = reader.nextLine();
-        System.out.println("What's your age?");
-        int age = Integer.valueOf(reader.nextLine());
-        System.out.println("What's your weight?");
-        double weight = Double.valueOf(reader.nextLine());
-        System.out.println("What's your height?");
-        double height = Double.valueOf(reader.nextLine());
+        paul.growOlder();
+        System.out.println(paul);
 
-        Person person = new Person(name, age);
-        System.out.println(person);
+        paul.growOlder(10);
+        System.out.println(paul);
     }
 }
