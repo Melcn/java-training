ea3a44910ed7d6ffe252953d048d67e63d1aef90
Melcn <melaniearinosy@outlook.com>
2023-11-20 09:55:14 +0100

    feat(wotrainig): init attribute n contructors in counter

--- src/WOtraining/Counter.java
+++ src/WOtraining/Counter.java
@@ -1,4 +1,16 @@
 package WOtraining;
 
 public class Counter {
+
+    private int startValue;
+
+    public Counter(int startValue) {
+        this.startValue = startValue;
+    }
+
+    public Counter() {
+        this.startValue = 0;
+    }
+
+    
 }
