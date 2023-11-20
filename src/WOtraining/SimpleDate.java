cc0fed267abd0ab2a6a6fb7895a9e147f00e36fb
Melcn <melaniearinosy@outlook.com>
2023-11-20 16:24:53 +0100

    feat(wotrainig): create simpleDate

---
+++ src/WOtraining/SimpleDate.java
@@ -0,0 +1,14 @@
+package WOtraining;
+
+public class SimpleDate {
+
+    private int day;
+    private int month;
+    private int year;
+
+    public SimpleDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
