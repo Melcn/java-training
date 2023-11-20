119e872ecd0e5484ac9bf4b9503b3ccc4aa38faa
Melcn <melaniearinosy@outlook.com>
2023-11-20 14:13:42 +0100

    feat(wotrainig): init AmusmentParkTest

--- src/WOtraining/AmusementParkTest.java
+++ src/WOtraining/AmusementParkTest.java
@@ -1,4 +1,13 @@
 package WOtraining;
 
 public class AmusementParkTest {
+    public static void main(String[] argv) {
+        Person matt = new Person("Matt");
+        matt.setWeight(86);
+        matt.setHeight(180);
+
+        Person jasper = new Person("Jasper");
+        jasper.setWeight(34);
+        jasper.setHeight(132);
+    }
 }
