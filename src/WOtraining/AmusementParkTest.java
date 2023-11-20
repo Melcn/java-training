ad102c358db50aaa10bb189821c5cad4d26ffd19
Melcn <melaniearinosy@outlook.com>
2023-11-20 14:14:59 +0100

    feat(wotrainig): test AmusmentPark

--- src/WOtraining/AmusementParkTest.java
+++ src/WOtraining/AmusementParkTest.java
@@ -11,5 +11,18 @@ public class AmusementParkTest {
         jasper.setHeight(132);
 
         AmusementParkRide waterTrack = new AmusementParkRide("Water track", 140);
+
+        if (waterTrack.allowedToRide(matt)) {
+            System.out.println(matt.getName() + " may enter the ride");
+        } else {
+            System.out.println(matt.getName() + " may not enter the ride");
+        }
+
+        if (waterTrack.allowedToRide(jasper)) {
+            System.out.println(jasper.getName() + " may enter the ride");
+        } else {
+            System.out.println(jasper.getName() + " may not enter the ride");
+        }
+
     }
 }
