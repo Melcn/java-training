bfb3d5afa791deb0b3b726df99161086fd718615
Melcn <melaniearinosy@outlook.com>
2023-11-20 09:07:27 +0100

    feat(wotrainig): create constructors in product

--- src/WOtraining/Product.java
+++ src/WOtraining/Product.java
@@ -5,10 +5,22 @@ public class Product {
     private String location;
     private int weight;
 
-    public Product(String name, double location, int weight) {
+    public Product(String name) {
         this.name = name;
-        this.weight = weight;
+        this.location = "etagere";
+        this.weight = 1;
+    }
+
+    public Product(String name, String location) {
+        this.name = name;
         this.location = location;
+        this.weight = 1;
+    }
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.location = "etagere";
+        this.weight = weight;
     }
 
     public void printProduct() {
