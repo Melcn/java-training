65b54496e658dabce1c30e10e7bb947bd0ec7aa9
Melcn <melaniearinosy@outlook.com>
2023-11-20 14:09:21 +0100

    feat(wotrainig): try nullpointerexception

--- src/WOtraining/PersonTest.java
+++ src/WOtraining/PersonTest.java
@@ -7,11 +7,14 @@ public class PersonTest {
     public static void main(String[] args) {
         Person paul = new Person("Paul", 24, 184, 70);
         Person joan = new Person("Joan Ball");
+
         System.out.println(paul);
 
         paul.growOlder();
         System.out.println(paul);
 
+        paul = null;
+        
         paul.growOlder(10);
         System.out.println(paul);
     }
