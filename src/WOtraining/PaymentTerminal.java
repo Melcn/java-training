674a22b349dd61f7155f545d48baa574eef6929c
Melcn <melaniearinosy@outlook.com>
2023-11-20 15:47:36 +0100

    feat(wotrainig): init attribute constructors in PaymentTerminal

--- src/WOtraining/PaymentTerminal.java
+++ src/WOtraining/PaymentTerminal.java
@@ -1,5 +1,13 @@
 package WOtraining;
 
 public class PaymentTerminal {
-    
+
+    private double money;
+    private int affordableMeals;
+
+    private int heartyMeals;
+
+    public PaymentTerminal() {
+        this.money = 1000;
+    }
 }
