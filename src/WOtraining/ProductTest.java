427d9f1213506a4e1d057568644fe56498fd0d74
Melcn <melaniearinosy@outlook.com>
2023-11-20 09:38:04 +0100

    feat(wotrainig): test new product

--- src/WOtraining/ProductTest.java
+++ src/WOtraining/ProductTest.java
@@ -3,8 +3,12 @@ package WOtraining;
 public class ProductTest {
     public static void main(String[] args) {
 
-        //  Product pr = new Product("Banane", 3, 5);
+        Product tapeMeasure = new Product("Tape measure");
+        Product plaster = new Product("Plaster", "home improvement section");
+        Product tyre = new Product("Tyre", 5);
 
-        // pr.printProduct();
+        tapeMeasure.printProduct();
+        plaster.printProduct();
+        tyre.printProduct();
     }
 }
