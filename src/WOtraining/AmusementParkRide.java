f8c06d9831da32374666b5b7dbc6d62b9fd8d771
Melcn <melaniearinosy@outlook.com>
2023-11-20 14:11:28 +0100

    feat(wotrainig): create AmusementParkRide class

--- src/WOtraining/AmusementParkRide.java
+++ src/WOtraining/AmusementParkRide.java
@@ -1,4 +1,15 @@
 package WOtraining;
 
 public class AmusementParkRide {
+    private String name;
+    private int lowestHeight;
+
+    public AmusementParkRide(String name, int lowestHeight) {
+        this.name = name;
+        this.lowestHeight = lowestHeight;
+    }
+
+    public String toString() {
+        return this.name + ", minimum height : " + this.lowestHeight;
+    }
 }
