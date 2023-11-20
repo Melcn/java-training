2d6f130e22ed86d3e73e124eab1d43e841aff742
Melcn <melaniearinosy@outlook.com>
2023-11-20 15:48:58 +0100

    feat(wotrainig): create eatAffordably() + toString in Payment Terminal

--- src/WOtraining/PaymentTerminal.java
+++ src/WOtraining/PaymentTerminal.java
@@ -10,4 +10,16 @@ public class PaymentTerminal {
     public PaymentTerminal() {
         this.money = 1000;
     }
+
+    public double eatAffordably(double payment) {
+        return 0.0;
+    }
+
+    public double eatHeartily(double payment) {
+        return 0.0;
+    }
+
+    public String toString() {
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
 }
