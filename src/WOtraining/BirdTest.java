43625b69e8acbca089bfaa5cfa54341aa00e4ea9
Melcn <melaniearinosy@outlook.com>
2023-11-21 10:15:47 +0100

    feat(wotraining): test Arraylists Birds in test

--- src/WOtraining/BirdTest.java
+++ src/WOtraining/BirdTest.java
@@ -8,13 +8,28 @@ public class BirdTest {
 
         ArrayList<Bird> birds = new ArrayList<>();
         Bird red = new Bird("Red");
-        System.out.println(red);
-
-        Bird chuck = new Bird("Chuck");
-        System.out.println(chuck);
-
-        if (red.equals(chuck)) {
-            System.out.println(red + " equals " + chuck);
+        if (birds.contains(red)) {
+            System.out.println("Red is on the list.");
+        } else {
+            System.out.println("Red is not on the list.");
         }
+
+        birds.add(red);
+        if (birds.contains(red)) {
+            System.out.println("Red is on the list.");
+        } else {
+            System.out.println("Red is not on the list.");
+        }
+
+
+        System.out.println("However!");
+
+        red = new Bird("Red");
+        if (birds.contains(red)) {
+            System.out.println("Red is on the list.");
+        } else {
+            System.out.println("Red is not on the list.");
+        }
+
     }
 }
