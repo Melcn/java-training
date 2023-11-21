a89b166e63b6748488aa8fab6c9b48db41f48eb6
Melcn <melaniearinosy@outlook.com>
2023-11-21 14:44:05 +0100

    feat(): create Menu and testmenu

---
+++ src/WOtraining/Menu.java
@@ -0,0 +1,23 @@
+package WOtraining;
+
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<String> meals;
+
+    public Menu() {
+        this.meals = new ArrayList<>();
+    }
+
+    public void addMeal(String meal) {
+
+    }
+
+    public String printMeals() {
+        return "meal";
+    }
+
+    public void clearMenu() {
+
+    }
+}
