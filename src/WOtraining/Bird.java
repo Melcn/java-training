43bc5ce733217e1ea0c61702023042d2e49e9033
Melcn <melaniearinosy@outlook.com>
2023-11-21 10:22:03 +0100

    feat(wotraining): create/update equals() in Bird

--- src/WOtraining/Bird.java
+++ src/WOtraining/Bird.java
@@ -6,4 +6,17 @@ public class Bird {
     public Bird(String name) {
         this.name = name;
     }
+
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+        if (!(compared instanceof Bird)) {
+            return false;
+        }
+
+        Bird comparedBird = (Bird) compared;
+
+        return this.name.equals(comparedBird.name);
+    }
 }
