10d6e85fbc81c9112e3b39a61408e2bd0c00484e
Melcn <melaniearinosy@outlook.com>
2023-11-21 09:28:42 +0100

    feat(wotraining): init apartment test

--- src/WOtraining/ApartmentTest.java
+++ src/WOtraining/ApartmentTest.java
@@ -4,5 +4,11 @@ public class ApartmentTest {
 
     public static void main(String[] args) {
 
+        Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
+        Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
+        Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
+
+        System.out.println(manhattanStudioApt.largerThan(atlantaTwoBedroomApt));
+        System.out.println(bangorThreeBedroomApt.largerThan(atlantaTwoBedroomApt));
     }
 }
