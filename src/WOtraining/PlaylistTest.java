843b6bd6bd6d6a76ab933895d51055a6c8e93d2e
Melcn <melaniearinosy@outlook.com>
2023-11-21 14:38:39 +0100

    feat(wotraining): test Playlist in tes

---
+++ src/WOtraining/PlaylistTest.java
@@ -0,0 +1,11 @@
+package WOtraining;
+
+public class PlaylistTest {
+
+    public static void main(String[] args) {
+        Playlist list = new Playlist();
+        list.addSong("Sorateiden kuningas");
+        list.addSong("Teuvo, maanteiden kuningas");
+        list.printSongs();
+    }
+}
