8184f3df8abf27ab2ec91b8657dd91704c142386
Melcn <melaniearinosy@outlook.com>
2023-11-21 16:37:35 +0100

    feat(): create messagingService

---
+++ src/WOtraining/MessagingService.java
@@ -0,0 +1,10 @@
+package WOtraining;
+
+import java.util.ArrayList;
+
+public class MessagingService {
+
+    private ArrayList<String> message = new ArrayList<>();
+    private String sender;
+    private String content;
+}
