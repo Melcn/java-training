ff3379840ab2933b03f90c8b71146cb9046b7047
Melcn <melaniearinosy@outlook.com>
2023-11-21 10:13:50 +0100

    feat(wotraining): init Bird in birdTest

--- src/WOtraining/BirdTest.java
+++ src/WOtraining/BirdTest.java
@@ -4,5 +4,14 @@ public class BirdTest {
 
     public static void main(String[] args) {
 
+        Bird red = new Bird("Red");
+        System.out.println(red);
+
+        Bird chuck = new Bird("Chuck");
+        System.out.println(chuck);
+
+        if (red.equals(chuck)) {
+            System.out.println(red + " equals " + chuck);
+        }
     }
 }
