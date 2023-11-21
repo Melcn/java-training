22e53198df9cd9136f3f5c5fe04cb625bba9ae6a
Melcn <melaniearinosy@outlook.com>
2023-11-21 10:14:44 +0100

    feat(wotraining): init Arraylists Birds in test

--- src/WOtraining/BirdTest.java
+++ src/WOtraining/BirdTest.java
@@ -1,9 +1,12 @@
 package WOtraining;
 
+import java.util.ArrayList;
+
 public class BirdTest {
 
     public static void main(String[] args) {
 
+        ArrayList<Bird> birds = new ArrayList<>();
         Bird red = new Bird("Red");
         System.out.println(red);
 
