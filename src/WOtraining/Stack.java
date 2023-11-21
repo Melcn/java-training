5703c59bad18d48c96f5660d64597cc68f466a5f
Melcn <melaniearinosy@outlook.com>
2023-11-21 15:21:44 +0100

    feat(): create new Stack class

---
+++ src/WOtraining/Stack.java
@@ -0,0 +1,16 @@
+package WOtraining;
+
+import java.util.ArrayList;
+
+public class Stack {
+
+    ArrayList<String> stacks = new ArrayList<>();
+
+    public Stack() {
+        this.stacks = new ArrayList<>();
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+}
