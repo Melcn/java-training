e54fa8a95ff5d8780b87a6fe14734dc8dbed8be8
Melcn <melaniearinosy@outlook.com>
2023-11-23 10:41:44 +0100

    create methods in Room

--- src/WOtraining/Room.java
+++ src/WOtraining/Room.java
@@ -9,4 +9,17 @@ public class Room {
     public Room() {
 
     }
+
+    public void add(Person person) {
+        //ajouter la personne passée en paramètre à la liste.
+    }
+
+    public boolean isEmpty() {
+        //indique si la pièce est vide ou non
+        return true;
+    }
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
 }
