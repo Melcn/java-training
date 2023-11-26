2bab11dbe4638581dea33d77aee539c9b61edb16
Melcn <melaniearinosy@outlook.com>
2023-11-26 22:22:08 +0100

    feat(Ã): init container

--- src/WOtraining/Part7/LiquidUserInterface.java
+++ src/WOtraining/Part7/LiquidUserInterface.java
@@ -18,5 +18,37 @@ public class LiquidUserInterface {
 
     public void start() {
 
+        while (true) {
+
+            String input = scanner.nextLine();
+            if (input.equals("quit")) {
+                break;
+            }
+
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+
+            System.out.println("First: " + firstContainer);
+            System.out.println("Second: " + secondContainer);
+
+            if (command.equals("add")) {
+                this.firstContainer.add(amount);
+            } else if (command.equals("move")) {
+                if ((this.firstContainer.contains() - amount) >= 0) {
+                    this.firstContainer.remove(amount);
+                    this.secondContainer.add(amount);
+                } else {
+                    int moveBuffer = this.firstContainer.contains();
+                    this.firstContainer.remove(this.firstContainer.contains());
+                    this.secondContainer.add(this.firstContainer.contains() + moveBuffer);
+                }
+            } else if (command.equals("remove")) {
+                this.secondContainer.remove(amount);
+            }
+
+        }
+        System.out.println("First: " + firstContainer);
+        System.out.println("Second: " + secondContainer);
     }
 }
