b0e6f00087855dc735b328ade63f5bb175601d3a
Melcn <melaniearinosy@outlook.com>
2023-11-27 10:41:25 +0100

    feat(trainingWO) : create PositiveNumber
    to Test streams values

---
+++ src/WOtraining/Part10/PositiveNumbers.java
@@ -0,0 +1,12 @@
+package WOtraining.Part10;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PositiveNumbers {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+    }
+}
