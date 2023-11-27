87368816ab7b74a99cc65211612900c5edac8fc0
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:32:27 +0100

    feat(moocFI): Test GradeStats package in Part7

--- src/MOOCFI2/Part7/Paradigms/GradeStats/Main.java
+++ src/MOOCFI2/Part7/Paradigms/GradeStats/Main.java
@@ -1,4 +1,11 @@
 package MOOCFI2.Part7.Paradigms.GradeStats;
 
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        UI interfacee = new UI(new GradeRegister(), new Scanner(System.in));
+        interfacee.start();
+    }
 }
