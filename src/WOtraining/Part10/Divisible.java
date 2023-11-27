38d7f973b1a1c7476acaf28dedbae78e378b793d
Melcn <melaniearinosy@outlook.com>
2023-11-27 11:39:33 +0100

    feat(trainingWo): init Divisible

--- src/WOtraining/Part10/Divisible.java
+++ src/WOtraining/Part10/Divisible.java
@@ -1,7 +1,23 @@
 package WOtraining.Part10;
 
+import java.util.ArrayList;
+
 public class Divisible {
     public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(-17);
+        numbers.add(-5);
+        numbers.add(7);
 
+        ArrayList<Integer> divisible = divisible(numbers);
+
+        divisible.stream()
+                .forEach(num -> System.out.println(num));
+    }
+
+    private static ArrayList<Integer> divisible(ArrayList<Integer> numbers) {
+        return numbers;
     }
 }
