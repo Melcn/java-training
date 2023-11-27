50fc8786a70d65add574ce241b8ba82bdaef2d99
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:30:45 +0100

    feat(moocFI): add methods add gets in graderegister for GradeStats package in Part7

--- src/MOOCFI2/Part7/Paradigms/GradeStats/GradeRegister.java
+++ src/MOOCFI2/Part7/Paradigms/GradeStats/GradeRegister.java
@@ -9,4 +9,57 @@ public class GradeRegister {
     public GradeRegister() {
         this.grades = new ArrayList<>();
     }
+
+    public void add(int grade) {
+        if (grade >= 0 && grade <= 100) {
+            this.grades.add(grade);
+        }
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public int getSize() {
+        return this.getSizeAboveGrade(0);
+    }
+
+    public int sum() {
+        return this.sumAboveGrade(0);
+    }
+
+    public double average() {
+        return this.averageGradesAboveGrade(0);
+    }
+    public int sumAboveGrade(int grade) {
+        int sum = 0;
+        for (int i = 0; i < this.grades.size(); i++) {
+            if (this.grades.get(i) >= grade) {
+                sum += this.grades.get(i);
+            }
+        }
+        return sum;
+    }
+
+    public int getSizeAboveGrade(int grade) {
+        int count = 0;
+        for (int i = 0; i < this.grades.size(); i++) {
+            if (this.grades.get(i) >= grade) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public double averageGradesAboveGrade(int grade) {
+        if (this.getSizeAboveGrade(grade) > 0) {
+            return (double) this.sumAboveGrade(grade) / this.getSizeAboveGrade(grade);
+        }
+        return -1;
+    }
+
+    public double percentageAboveGrade(int grade) {
+        return 100.0 * this.getSizeAboveGrade(grade) / this.getSize();
+    }
+
 }
