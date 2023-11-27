6a93abde14d5498369a32f083451f671e414701b
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:29:14 +0100

    feat(moocFI): init UI for GradeStats package in Part7

--- src/MOOCFI2/Part7/Paradigms/GradeStats/UI.java
+++ src/MOOCFI2/Part7/Paradigms/GradeStats/UI.java
@@ -1,4 +1,15 @@
 package MOOCFI2.Part7.Paradigms.GradeStats;
 
+import java.util.Scanner;
+
 public class UI {
+
+    private GradeRegister register;
+    private Scanner scanner;
+
+    public UI(GradeRegister register, Scanner scanner) {
+        this.register = register;
+        this.scanner = scanner;
+    }
+
 }
