5035621082655bf0e00ff15392dd23cde6cec90b
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:20:40 +0100

    feat(moocFI): create Test ReadyMadeSorting in Part7

---
+++ src/MOOCFI2/Part7/Paradigms/ReadyMadeSortingAlgo/Test.java
@@ -0,0 +1,9 @@
+package MOOCFI2.Part7.Paradigms.ReadyMadeSortingAlgo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // insert test code here
+        int[] array = {3, 1, 5, 99, 3, 12};
+    }
+}
