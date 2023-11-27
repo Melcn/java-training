f6ece2b09098de4609dd073025d4ff7995d7c785
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:21:10 +0100

    feat(moocFI): test Test ReadyMadeSorting in Part7

--- src/MOOCFI2/Part7/Paradigms/ReadyMadeSortingAlgo/Test.java
+++ src/MOOCFI2/Part7/Paradigms/ReadyMadeSortingAlgo/Test.java
@@ -1,9 +1,18 @@
 package MOOCFI2.Part7.Paradigms.ReadyMadeSortingAlgo;
 
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
         // insert test code here
         int[] array = {3, 1, 5, 99, 3, 12};
+    }
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+
+    public static void sort(String[] array) {
+        Arrays.sort(array);
     }
 }
