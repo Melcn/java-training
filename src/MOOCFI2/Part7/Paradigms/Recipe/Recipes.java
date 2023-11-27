8f757d54cd587f4dea1a413afdf89705f68aff80
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:36:26 +0100

    feat(moocFI): init Recipesfor Recipe package in Part7

--- src/MOOCFI2/Part7/Paradigms/Recipe/Recipes.java
+++ src/MOOCFI2/Part7/Paradigms/Recipe/Recipes.java
@@ -1,4 +1,33 @@
 package MOOCFI2.Part7.Paradigms.Recipe;
 
+import java.util.ArrayList;
+
 public class Recipes {
+
+    private String name;
+    private int time;
+    private ArrayList<String> ingredients;
+
+    public Recipes(String name, int time, ArrayList<String> ingredients) {
+        this.name = name;
+        this.time = time;
+        this.ingredients = ingredients;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public String toString() {
+        return this.name + ", cooking time: " + this.time;
+    }
+
 }
