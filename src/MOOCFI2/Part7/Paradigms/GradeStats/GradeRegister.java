41aa5531245d97843dda81f866c522b9794d5b8e
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:28:00 +0100

    feat(moocFI): init GradeRegister  for GradeStats package in Part7

--- src/MOOCFI2/Part7/Paradigms/GradeStats/GradeRegister.java
+++ src/MOOCFI2/Part7/Paradigms/GradeStats/GradeRegister.java
@@ -1,4 +1,12 @@
 package MOOCFI2.Part7.Paradigms.GradeStats;
 
+import java.util.ArrayList;
+
 public class GradeRegister {
+
+    private ArrayList<Integer> grades;
+
+    public GradeRegister() {
+        this.grades = new ArrayList<>();
+    }
 }
