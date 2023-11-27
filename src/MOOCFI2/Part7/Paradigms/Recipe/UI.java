484b5e8a13c83e93d0689b19eca57ab4a468c7e2
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:35:46 +0100

    feat(moocFI): update start() in UI for Recipe package in Part7

--- src/MOOCFI2/Part7/Paradigms/Recipe/UI.java
+++ src/MOOCFI2/Part7/Paradigms/Recipe/UI.java
@@ -1,5 +1,6 @@
 package MOOCFI2.Part7.Paradigms.Recipe;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UI {
@@ -11,5 +12,15 @@ public class UI {
         this.inputDocument = new Read(this.scanner.nextLine());
     }
 
-    public void start() {}
+    public void start() {
+        ArrayList<Recipes> listedDocument = this.inputDocument.docToRecipe();
+
+        System.out.println("Commands:");
+        System.out.println("list - lists the recipes");
+        System.out.println("stop - stops the program");
+        System.out.println("find name - searches recipes by name");
+        System.out.println("find cooking time - searches recipes by cooking time");
+        System.out.println("find ingredient - searches recipes by ingredient");
+        System.out.println("");
+    }
 }
