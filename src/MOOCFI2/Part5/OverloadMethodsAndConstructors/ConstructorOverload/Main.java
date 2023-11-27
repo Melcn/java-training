5ced89ee8b4caa9c55f08a64fa3641e9b02bd7e5
Melcn <melaniearinosy@outlook.com>
2023-11-27 19:00:00 +0100

    feat(moocFI): Update ConstrcutorOverload in Part5

--- src/MOOCFI2/Part5/OverloadMethodsAndConstructors/ConstructorOverload/Main.java
+++ src/MOOCFI2/Part5/OverloadMethodsAndConstructors/ConstructorOverload/Main.java
@@ -1,14 +1,14 @@
 package MOOCFI2.Part5.OverloadMethodsAndConstructors.ConstructorOverload;
 
-public class ProductTest {
+public class Main {
     public static void main(String[] args) {
-
+        // Test your class here
         Product tapeMeasure = new Product("Tape measure");
         Product plaster = new Product("Plaster", "home improvement section");
         Product tyre = new Product("Tyre", 5);
 
-        tapeMeasure.printProduct();
-        plaster.printProduct();
-        tyre.printProduct();
+        System.out.println(tapeMeasure);
+        System.out.println(plaster);
+        System.out.println(tyre);
     }
 }
