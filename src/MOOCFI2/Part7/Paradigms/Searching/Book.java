f0277165df3b016e2fe5d05c49b1fc9e02207cf1
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:24:16 +0100

    feat(moocFI): add accessors in  Book for searching package in Part7

--- src/MOOCFI2/Part7/Paradigms/Searching/Book.java
+++ src/MOOCFI2/Part7/Paradigms/Searching/Book.java
@@ -8,4 +8,17 @@ public class Book {
         this.id = id;
         this.name = name;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "(id: " + id + "; name: " + name + ")";
+    }
 }
