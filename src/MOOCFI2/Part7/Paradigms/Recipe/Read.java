323514a2f1fdaf94b60a52eebaabf395af70e57b
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:38:37 +0100

    feat(moocFI): init Read for Recipe package in Part7

--- src/MOOCFI2/Part7/Paradigms/Recipe/Read.java
+++ src/MOOCFI2/Part7/Paradigms/Recipe/Read.java
@@ -1,4 +1,15 @@
 package MOOCFI2.Part7.Paradigms.Recipe;
 
+import java.util.ArrayList;
+
 public class Read {
+
+    private String file;
+
+    public Read(String file) {
+        this.file = file;
+    }
+
+    public ArrayList<Recipes> docToRecipe() {
+        ArrayList<Recipes> recipes = new ArrayList<>();
 }
