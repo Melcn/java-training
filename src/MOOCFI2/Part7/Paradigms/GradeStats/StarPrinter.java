fea57dc7173c04a8f01e8e06b70f0723c811d8dc
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:28:33 +0100

    feat(moocFI): init StarPrinter for GradeStats package in Part7

--- src/MOOCFI2/Part7/Paradigms/GradeStats/StarPrinter.java
+++ src/MOOCFI2/Part7/Paradigms/GradeStats/StarPrinter.java
@@ -1,4 +1,18 @@
 package MOOCFI2.Part7.Paradigms.GradeStats;
 
 public class StarPrinter {
+
+    public StarPrinter() {
+    }
+
+    public void printer(int[] points) {
+        for (int i = (points.length - 1); i >= 0; i--) {
+            String stars = "";
+            int count = points[i];
+            for (int j = count; j > 0; j--) {
+                stars += "*";
+            }
+            System.out.println(i + ": " + stars);
+        }
+    }
 }
