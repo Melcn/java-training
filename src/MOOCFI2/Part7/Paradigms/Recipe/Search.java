cd1bba4408a95b47becc36fdd7ac914785cbf591
Melcn <melaniearinosy@outlook.com>
2023-11-27 20:37:38 +0100

    feat(moocFI): init Maintest for Recipe package in Part7

--- src/MOOCFI2/Part7/Paradigms/Recipe/Search.java
+++ src/MOOCFI2/Part7/Paradigms/Recipe/Search.java
@@ -1,4 +1,16 @@
 package MOOCFI2.Part7.Paradigms.Recipe;
 
+import java.util.Scanner;
+
 public class Search {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("File to read: ");
+
+        UI ui = new UI(scanner);
+        ui.start();
+
+    }
 }
