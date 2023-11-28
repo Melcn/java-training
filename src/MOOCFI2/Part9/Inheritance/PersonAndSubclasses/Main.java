c0a49b5de029ca1031c183b05b969c47d480e4bb
Melcn <melaniearinosy@outlook.com>
2023-11-28 15:48:50 +0100

    feat(moocFI): add separators in testMain for Inheritance test in Part9

--- src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Main.java
+++ src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Main.java
@@ -7,22 +7,38 @@ public class Main {
         System.out.println(ada);
         System.out.println(esko);
 
+        System.out.println("  ");
+        System.out.println(" ******************************************* ");
+        System.out.println("  ");
+
         Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
         System.out.println(ollie);
         System.out.println("Study credits " + ollie.credits());
         ollie.study();
         System.out.println("Study credits " + ollie.credits());
 
+        System.out.println("  ");
+        System.out.println(" ******************************************* ");
+        System.out.println("  ");
+
         Student allie = new Student("Allie", "6381 Hollywood Blvd. Los Angeles 90028");
         System.out.println(allie);
         allie.study();
         System.out.println(allie);
+
+        System.out.println("  ");
+        System.out.println(" ******************************************* ");
+        System.out.println("  ");
 
         Teacher adaa = new Teacher("Adaa Lovelace", "24 Maddox St. London W1S 2QN", 1200);
         Teacher eesko = new Teacher("Eesko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
         System.out.println(adaa);
         System.out.println(eesko);
 
+        System.out.println("  ");
+        System.out.println(" ******************************************* ");
+        System.out.println("  ");
+        
         Student olllie = new Student("Olllie", "6381 Hollywood Blvd. Los Angeles 90028");
 
         int i = 0;
