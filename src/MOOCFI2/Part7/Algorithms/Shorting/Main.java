fb91e96dc34ed17fe626de0e240a52d0e9cc01a3
Melcn <melaniearinosy@outlook.com>
2023-11-28 10:50:09 +0100

    feat(MoocFi): create shorting main classes in part7

---
+++ src/MOOCFI2/Part7/Algorithms/Shorting/Main.java
@@ -0,0 +1,8 @@
+package MOOCFI2.Part7.Algorithms.Shorting;
+
+public class Main {
+
+    public static int smallest(int[] array) {
+        return 0;
+    }
+}
---
+++ src/MOOCFI2/Part7/Algorithms/Shorting/Test.java
@@ -0,0 +1,9 @@
+package MOOCFI2.Part7.Algorithms.Shorting;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] numbers = {6, 5, 8, 7, 11};
+        System.out.println("Smallest: " + Main.smallest(numbers));
+    }
+
+}
