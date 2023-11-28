73a09bebc4fb019b3f765c08f3d6e7fd5fce01c1
Melcn <melaniearinosy@outlook.com>
2023-11-28 15:26:35 +0100

    feat(moocFI): create Student extends Person for Inheritance test in Part9

---
+++ src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Student.java
@@ -0,0 +1,8 @@
+package MOOCFI2.Part9.Inheritance.PersonAndSubclasses;
+
+public class Student extends Person {
+
+    public Student(String name, String adress) {
+        super(name, adress);
+    }
+}
