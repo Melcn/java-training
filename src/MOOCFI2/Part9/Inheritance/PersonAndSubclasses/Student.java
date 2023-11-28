ea661db0829af61831a4f309d4f8dfd92580ee86
Melcn <melaniearinosy@outlook.com>
2023-11-28 15:32:01 +0100

    feat(moocFI): tests studycredit() in Main for Student extends Person for Inheritance test in Part9

--- src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Student.java
+++ src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Student.java
@@ -2,7 +2,18 @@ package MOOCFI2.Part9.Inheritance.PersonAndSubclasses;
 
 public class Student extends Person {
 
+    public int credit;
+
     public Student(String name, String adress) {
         super(name, adress);
+        this.credit = 0;
+    }
+
+    public int credits() {
+        return credit;
+    }
+
+    public void study() {
+        credit++;
     }
 }
