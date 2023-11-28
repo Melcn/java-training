94e4cc6ac7da03ae2416bd380a36879fbe38ebc0
Melcn <melaniearinosy@outlook.com>
2023-11-28 15:21:29 +0100

    feat(moocFI): create Person class for Inheritance test in Part9

---
+++ src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Person.java
@@ -0,0 +1,12 @@
+package MOOCFI2.Part9.Inheritance.PersonAndSubclasses;
+
+public class Person {
+
+    private String name;
+    private String adress;
+
+    public Person(String name, String adress) {
+        this.name = name;
+        this.adress = adress;
+    }
+}
