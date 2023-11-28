2941aca0d64cd89efc29fc341f179bc94d1a21bc
Melcn <melaniearinosy@outlook.com>
2023-11-28 15:44:42 +0100

    feat(moocFI): create toString() + getSalary() in Teacher extends Person for Inheritance test in Part9

--- src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Teacher.java
+++ src/MOOCFI2/Part9/Inheritance/PersonAndSubclasses/Teacher.java
@@ -2,7 +2,19 @@ package MOOCFI2.Part9.Inheritance.PersonAndSubclasses;
 
 public class Teacher extends Person {
 
-    public Teacher(String name, String adress) {
+    private int salary;
+
+    public Teacher(String name, String adress, int salary) {
         super(name, adress);
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + this.getSalary();
     }
 }
