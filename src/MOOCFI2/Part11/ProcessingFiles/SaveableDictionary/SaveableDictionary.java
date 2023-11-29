650e62a30ad1cc5b1d1f826009e40ed938bd41ca
Melcn <melaniearinosy@outlook.com>
2023-11-29 11:21:17 +0100

    feat(moocFI): init for SaveableDictionary part11

--- src/MOOCFI2/Part11/ProcessingFiles/SaveableDictionary/SaveableDictionary.java
+++ src/MOOCFI2/Part11/ProcessingFiles/SaveableDictionary/SaveableDictionary.java
@@ -1,4 +1,19 @@
 package MOOCFI2.Part11.ProcessingFiles.SaveableDictionary;
 
+import java.io.File;
+import java.util.HashMap;
+
 public class SaveableDictionary {
+
+    private HashMap<String, String> dictionary;
+    private File file;
+
+    public SaveableDictionary() {
+        this.dictionary = new HashMap<>();
+    }
+
+    public SaveableDictionary(String fileName) {
+        this.dictionary = new HashMap<>();
+        this.file = new File(fileName);
+    }
 }
