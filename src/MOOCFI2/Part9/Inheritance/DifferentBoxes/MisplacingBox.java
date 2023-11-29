c98fefccef070dff2b372cf48cc7a9220d2182ee
Melcn <melaniearinosy@outlook.com>
2023-11-29 11:14:26 +0100

    feat(moocFI): init MisplacingBox for DifferentBoxes Inheritance in Part9

--- src/MOOCFI2/Part9/Inheritance/DifferentBoxes/MisplacingBox.java
+++ src/MOOCFI2/Part9/Inheritance/DifferentBoxes/MisplacingBox.java
@@ -1,4 +1,20 @@
 package MOOCFI2.Part9.Inheritance.DifferentBoxes;
 
-public class MisplacingBox {
+import java.util.ArrayList;
+
+public class MisplacingBox extends Box{
+
+    private ArrayList<Item> items;
+
+    public MisplacingBox() {
+        this.items = new ArrayList<>();
+    }
+
+    public void add(Item item) {
+        items.add(item);
+    }
+
+    public boolean isInBox(Item item) {
+        return false;
+    };
 }
