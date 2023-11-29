4c0c80703b5a038214f68fa2367965d9206d6b13
Melcn <melaniearinosy@outlook.com>
2023-11-29 11:10:03 +0100

    feat(moocFI): init Item for DifferentBoxes Inheritance in Part9

--- src/MOOCFI2/Part9/Inheritance/DifferentBoxes/Item.java
+++ src/MOOCFI2/Part9/Inheritance/DifferentBoxes/Item.java
@@ -1,4 +1,15 @@
 package MOOCFI2.Part9.Inheritance.DifferentBoxes;
 
 public class Item {
+    private String name;
+    private int weight;
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public Item(String name) {
+        this(name, 0);
+    }
 }
