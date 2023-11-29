bb19af4e4c9d6648aa1f5c763fc7b5bb842893e1
Melcn <melaniearinosy@outlook.com>
2023-11-29 11:21:59 +0100

    feat(moocFI): init Load() for SaveableDictionary part11

--- src/MOOCFI2/Part11/ProcessingFiles/SaveableDictionary/SaveableDictionary.java
+++ src/MOOCFI2/Part11/ProcessingFiles/SaveableDictionary/SaveableDictionary.java
@@ -1,7 +1,9 @@
 package MOOCFI2.Part11.ProcessingFiles.SaveableDictionary;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class SaveableDictionary {
 
@@ -15,5 +17,21 @@ public class SaveableDictionary {
     public SaveableDictionary(String fileName) {
         this.dictionary = new HashMap<>();
         this.file = new File(fileName);
+    }
+
+    public boolean load() {
+        try {
+            Scanner fileReader = new Scanner(Paths.get(this.file.getName()));
+            while (fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                String[] parts = line.split(":");
+                this.dictionary.putIfAbsent(parts[0], parts[1]);
+            }
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("File load error: " + e.getMessage());
+            return false;
+        }
     }
 }
