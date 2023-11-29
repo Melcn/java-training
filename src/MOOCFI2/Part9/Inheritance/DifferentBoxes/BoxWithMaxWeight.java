cdab0361ba80eab7c3a8073b9c2b7d3bf88afceb
Melcn <melaniearinosy@outlook.com>
2023-11-29 11:12:56 +0100

    feat(moocFI): init BxoMaxWeight for DifferentBoxes Inheritance in Part9

--- src/MOOCFI2/Part9/Inheritance/DifferentBoxes/BoxWithMaxWeight.java
+++ src/MOOCFI2/Part9/Inheritance/DifferentBoxes/BoxWithMaxWeight.java
@@ -1,4 +1,13 @@
 package MOOCFI2.Part9.Inheritance.DifferentBoxes;
 
-public class BoxWithMaxWeight {
+import java.util.ArrayList;
+
+public class BoxWithMaxWeight extends Box{
+    private int capacity;
+    private ArrayList<Item> items;
+
+    public BoxWithMaxWeight(int capacity) {
+        this.capacity = capacity;
+        this.items = new ArrayList<>();
+    }
 }
