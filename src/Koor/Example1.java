05afb3f034e398b69227febf9752f921358cced7
Melcn <melaniearinosy@outlook.com>
2023-12-11 16:38:33 +0100

    feat(koorinterfaces): create main example1

--- src/Koor/Example1.java
+++ src/Koor/Example1.java
@@ -1,6 +1,7 @@
 package Koor;
 
 import javax.swing.*;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 
 public class Example1 extends JFrame {
     private static final long serialVersionUID = -4939544011287453046L;
@@ -9,4 +10,12 @@ public class Example1 extends JFrame {
     private JButton btnPushMe = new JButton("Push me!");
     private JButton btnActivateMe = new JButton("Activate me!");
 
+    public static void main(String[] args) throws Exception {
+
+        UIManager.setLookAndFeel(new NimbusLookAndFeel());
+
+
+        Example1 demo = new Example1();
+        demo.setVisible(true);
+    }
 }
