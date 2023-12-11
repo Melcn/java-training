360db795f38e5a10393e4db8932fce4c7ca8701e
Melcn <melaniearinosy@outlook.com>
2023-12-11 15:59:33 +0100

    feat(): init grafik interface jframe

--- src/Koor/Example0.java
+++ src/Koor/Example0.java
@@ -1,4 +1,16 @@
 package Koor;
 
-public class Example0 {
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Example0 extends JFrame {
+
+    private static final long serialVersionUID = -4939544011287453046L;
+
+    private JButton btnClickMe = new JButton("Click me!");
+    private JButton btnPushMe = new JButton("Push me!");
+    private JButton btnActivateMe = new JButton("Activate me!");
 }
