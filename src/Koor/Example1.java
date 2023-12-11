8d9d9cc13f5533c7fa5cc30782dabd727bb242b7
Melcn <melaniearinosy@outlook.com>
2023-12-11 16:37:52 +0100

    feat(koorinterfaces): create new example1

---
+++ src/Koor/Example1.java
@@ -0,0 +1,12 @@
+package Koor;
+
+import javax.swing.*;
+
+public class Example1 extends JFrame {
+    private static final long serialVersionUID = -4939544011287453046L;
+
+    private JButton btnClickMe = new JButton("click me");
+    private JButton btnPushMe = new JButton("Push me!");
+    private JButton btnActivateMe = new JButton("Activate me!");
+
+}
