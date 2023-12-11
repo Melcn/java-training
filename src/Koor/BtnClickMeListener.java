71db1a1463c32e22a572acaa23b80a0213d587db
Melcn <melaniearinosy@outlook.com>
2023-12-11 16:41:34 +0100

    feat(koorinterfaces): create btnClickme

---
+++ src/Koor/BtnClickMeListener.java
@@ -0,0 +1,11 @@
+package Koor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BtnClickMeListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+}
