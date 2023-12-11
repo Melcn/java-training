2706e073ccba3c3fbf2f00d75f8eb6f5d011a1fe
Melcn <melaniearinosy@outlook.com>
2023-12-11 16:01:46 +0100

    feat(): create exemple in grafik interface jframe

--- src/Koor/Example0.java
+++ src/Koor/Example0.java
@@ -13,12 +13,27 @@ public class Example0 extends JFrame {
     private JButton btnPushMe = new JButton("Push me!");
     private JButton btnActivateMe = new JButton("Activate me!");
 
+    public Example0() {
+        super("Implémentation d'interface");
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        JPanel contentPane = (JPanel) this.getContentPane();
+        // Un FlowLayout permet de positionner les boutons les uns à la suite des autres.
+        contentPane.setLayout(new FlowLayout());
+        contentPane.add(btnClickMe);
+        contentPane.add(btnPushMe);
+        contentPane.add(btnActivateMe);
+
+        this.setSize(400, 200);
+        this.setLocationRelativeTo(null);
+    }
+
     public static void main(String[] args) throws Exception {
 
         UIManager.setLookAndFeel(new NimbusLookAndFeel());
 
 
-        Demo demo = new Demo();
+        Example0 demo = new Example0();
         demo.setVisible(true);
     }
 }
