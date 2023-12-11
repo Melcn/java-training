993395b878793fbe9d47d40308bb69ad568b2582
Melcn <melaniearinosy@outlook.com>
2023-12-11 16:57:56 +0100

    feat(koorinterfaces): update action performed to update title*

--- src/Koor/BtnPushMeListener.java
+++ src/Koor/BtnPushMeListener.java
@@ -1,11 +1,19 @@
 package Koor;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class BtnPushMeListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println("clickmeee!");
+        JButton btn = (JButton) event.getSource();
+        Example1 frame = (Example1) btn.getTopLevelAncestor();
+        frame.setTitle("Click");
+
+        frame.btnActivateMe.setText("Hello");
+        frame.btnActivateMe.setForeground(Color.red);
+
     }
 }
