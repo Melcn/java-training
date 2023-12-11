28e0f0d011fb1b9b2de852a91be8ca201609c85f
Melcn <melaniearinosy@outlook.com>
2023-12-11 20:13:54 +0100

    feat(koorinterfaces): create innerClass

--- src/Koor/Example2.java
+++ src/Koor/Example2.java
@@ -3,12 +3,15 @@ package Koor;
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Example2 extends JFrame {
     private static final long serialVersionUID = -4939544011287453046L;
     public JButton btnActivateMe = new JButton("Activate me!");
     private JButton btnClickMe = new JButton("click me");
     private JButton btnPushMe = new JButton("Push me!");
+    private int demo = 10;
 
     public Example2() {
         super("Implémentation d'interface");
@@ -19,7 +22,7 @@ public class Example2 extends JFrame {
         contentPane.add(btnClickMe);
         contentPane.add(btnPushMe);
         contentPane.add(btnActivateMe);
-        
+
         this.setSize(600, 400);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
@@ -27,5 +30,14 @@ public class Example2 extends JFrame {
 
     public static void main(String[] args) {
         new Example2();
+    }
+
+    private class BtnClickMeListenerInner implements ActionListener {
+
+        private int demo = 2000;
+
+        public void actionPerformed(ActionEvent e) {
+
+        }
     }
 }
