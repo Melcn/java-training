e68dd9256ab1a770f02e8b6f2a2e14a95f3e589f
Melcn <melaniearinosy@outlook.com>
2023-12-21 14:25:11 +0100

    feat(QuestionsMarks): init

--- src/easy/QuestionsMarks.java
+++ src/easy/QuestionsMarks.java
@@ -1,7 +1,16 @@
 package easy;
 
+import java.util.Scanner;
+
 public class QuestionsMarks {
+    public static String QuestionsMarks(String str) {
+        // code goes here
+        return str;
+    }
+
     public static void main(String[] args) {
-        
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print(QuestionsMarks(s.nextLine()));
     }
 }
